19e3a181eecd3d1c82f38a5879cdb4aeb19569d9
Roman Tuomisto <romantuomisto@Romans-MacBook-Air.local>
2019-10-11 09:39:34 +0300

    Updates

---
+++ precache-manifest.ac37115ec8531088e8ede83c4079e89b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59cacf8c8556e7718d8ef588dadeb51b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38f7e90015cd46b420d6",
+    "url": "/static/css/main.1aeb1b2e.chunk.css"
+  },
+  {
+    "revision": "05edb2c218f55023ff3b",
+    "url": "/static/js/2.b8b41457.chunk.js"
+  },
+  {
+    "revision": "38f7e90015cd46b420d6",
+    "url": "/static/js/main.a1ac12f5.chunk.js"
+  },
+  {
+    "revision": "7029bd24cfdcd024c8e1",
+    "url": "/static/js/runtime-main.a552c246.js"
+  }
+]);
